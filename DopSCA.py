741bdfc045c639ab3b32f86f7c1f649052648dc7
owenodriscoll <owenodriscoll1@gmail.com>
2023-09-19 13:31:56 +0200

    Included squint and changed geophysical doppler calculation

--- DopSCA.py
+++ DopSCA.py
@@ -1,31 +1,36 @@
 #%%
-from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from beam_pattern import beam_pattern_oneway, azimuth_beamwidth
 
 #%% 
 Lambda = 5.6e-2 # radio wavelength, m
 Vs = 6800 # velocity radar, m/s
 antenna_length = 10 # m
-theta_squint = 0 # squint angle
-footprint_az = 100e3 # azimuth fooprint
-inc = 37 # degrees
+theta_squint = np.deg2rad(0) # squint angle
+footprint_az = 100e3 # azimuth footprint, m
+inc = np.deg2rad(37) # incidence angle, degrees
 R = 700e3 # elevation satellite 
-az_samples = 200
+az_samples = 201 # samples along azimuth footprint
 
 #%% Simplified rectilinear geometry-induced Doppler with no Earth rotation
-# NOTE these equations assume a rectilinear geometry (parallel earth and flight path) with no squint
+# NOTE these equations assume a rectilinear geometry (parallel earth and flight path) 
+# NOTE we assume an along-track line on the surface with constant 0-Doppler range
+# NOTE we assume no squint
+# NOTE we assume a idealized and unweighted in azimuth (?) beam pattern
+# NOTE we assume no beam mispoining
 
-R_0 = R /np.cos(inc*np.pi/180) # slant range 
+R_0 = R /np.cos(inc) # slant range 
 
-az_beamwidth_max = np.arctan(footprint_az / R_0) # rad
-az_beamwidth = np.linspace(az_beamwidth_max/2, -az_beamwidth_max/2, az_samples) # rad
-R_azimuth = R_0 / np.cos(az_beamwidth) # range along azimuth beam for single slant range
+ground_azimuth_distance_squint_offstet = np.tan(theta_squint) * R_0 # azimuth squint offset on ground in meter
+ground_azimuth_distance = np.linspace(footprint_az/2, -footprint_az/2, az_samples) + ground_azimuth_distance_squint_offstet # m
+az_beamwidth = np.arctan(ground_azimuth_distance / R_0) # rad
+R_azimuth = R_0 / np.cos(az_beamwidth) # slant range along azimuth beam for single azimuth line
 
 f_Dop_geom = 2 * Vs * np.sin(az_beamwidth) / Lambda # hz, eq. 4.34 modified for all angles, not only Doppler centroid 
 B_Dop = 0.886 * 2 * Vs * np.cos(theta_squint) / antenna_length # hz, eq. 4.36 NOTE this is only the Doppler bandwidth within 3dB of beamform peak 
-Ka = 2 * Vs**2 * np.cos(theta_squint)**3 / (Lambda * R_azimuth) # Doppler frequency modulation rate, hz/s eq 4.38 
+Ka = 2 * Vs**2 * np.cos(az_beamwidth)**3 / (Lambda * R_azimuth) # NOTE should this be **2  or **3? Doppler frequency modulation rate, hz/s eq 4.38 
 
 az_beamwidth_3dB = azimuth_beamwidth(
         Lambda = Lambda,
@@ -33,11 +38,10 @@ az_beamwidth_3dB = azimuth_beamwidth(
         ) # rad
 
 beam_pattern_twoway = beam_pattern_oneway(
-    theta = az_beamwidth,
+    theta = az_beamwidth - theta_squint,
     azimuth_beamwidth = az_beamwidth_3dB
         )**2 
 
-ground_azimuth_distance = R_0 * np.tan(az_beamwidth)
 beam_patter_twoway_db = 10*np.log10(beam_pattern_twoway)
 
 #%%
@@ -62,26 +66,30 @@ ax2.set_xlabel("Along track distance [m]")
 
 fig.tight_layout()
 
-# %%
+# %% Geophysical Dopplers
 from surface_doppler import Doppler_inc
 
 # NOTE we assume points off the Doppler centroid can be modeled with a slightly different incidence angle
 # NOTE we still assume a flat Earth and rectilinear path
-# NOTE we assume the beam pattern can be applied to the geophysical Doppler
 # NOTE we assume no surface current are present
+# TODO find source of discontinuity/jumping values in geophysical Doppler a.f.o. along track
 
-inc_effective = np.arccos(R / R_azimuth) # this account for the slightly increasing incidence angle as a function of slant range away from beam center
-Dopplers = np.zeros((len(inc_effective), 7)) 
+inc_effective = np.arccos(R / R_azimuth) # rad, this account for the slightly increasing incidence angle as a function of slant range away from beam center
 phi_w = np.pi/6 # wind direction w.r.t. sensor
 phi_w_effective =  phi_w - az_beamwidth # corect for th fact that he relative wind direction changes along beam azimuth
+k_r = 2 * np.pi/ Lambda  # radar wave number , rad/m
+u_10 = 5 # mean wind speed, m/s
+fetch = 10e5 # fetch length for the non-equilibrium part of the spectrum, m
 
-for i, (inc_e, phi_e) in enumerate(tqdm(zip(inc_effective, phi_w_effective))):
-    
-    Dopplers[i, :] = Doppler_inc(inc_e, phi_w = phi_e)
+Doppler_v_VV = np.empty(len(inc_effective)) 
+for i, (inc_e, phi_w_e) in enumerate(tqdm(zip(inc_effective, phi_w_effective))):
+    Doppler_v_VV[i] = Doppler_inc(inc = inc_e, 
+                                  phi_w = phi_w_e, 
+                                  k_r = k_r,
+                                  u_10 = u_10, 
+                                  fetch = fetch)
 
-# c_sp_bar, c_wb_bar, c_br_bar, c_sp, c_wb, c_br_vv, c_br_hh = Doppler_inc()
-V_geo_vv = np.sum(Dopplers[:, :6], axis=1) # first 6 contributions are geophysical VV
-f_Dop_geo = 2 / Lambda * V_geo_vv * np.sin(inc_effective) # convert to LOS velocity and geophysical Doppler
+f_Dop_geo = -2 / Lambda * Doppler_v_VV * np.sin(inc_effective) # convert to LOS velocity and geophysical Doppler
 
 #%%
 
--- surface_doppler.py
+++ surface_doppler.py
@@ -1,9 +1,7 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from stereoid.oceans.forward_models.backscatter import backscatter_Kudry2023
 from stereoid.oceans.forward_models.Doppler import DopRIM2023_DP
+from stereoid.oceans.forward_models.backscatter import backscatter_Kudry2023
 from stereoid.oceans.waves.wave_spectra import Kudry_spec
-
 #%%
 # wavelengths and wave numbers
 g=9.81
@@ -22,8 +20,6 @@ omega=np.where(k > 0, np.sqrt(g*k), 0)
 phi = np.arctan2( k_y, k_x )  # 0 is cross-track direction waves, 90 along-track
 dks = np.outer( dk, dk )  # patch size
 
-u_10 = 7 # wind speed
-fetch = 10e3 # fetch length for the non-equilibrium part of the spectrum
 # phi_w = np.deg2rad(45) # wind direction
 # dks = (np.pi*2/100)**2 # two-dimensional grid resolution
 S=0 # Cartesian long-wave spectrum (non-equilibrium waves), optional: if set, set fetch=0
@@ -35,14 +31,45 @@ class temporary_class:
         self.inc_m = inc_m
         pass
 
-def Doppler_inc(inc, phi_w):
+def Doppler_inc(inc, phi_w, k_r, u_10, fetch):
+    """
+    Input
+    -----
+    inc: incidence angle, rad
+    phi_w: winddirection w.r.t. radar,rad
+    k_r: radar wave number, rad /m
+    u_10: wind speed at 10m eleveation, m/s 
+    fetch: fetch length for the non-equilibrium part of the spectrum, m
+    
+    Returns
+    -------
+
+    """
 
     obs_geo = temporary_class(inc_m=inc)
 
+    # calculate curvature spectrum
     B,_,_,_=Kudry_spec(k_x, k_y, u_10, fetch, phi_w, dks)
 
+    # convert to elevation spectrum
     S=np.where(k>0,B*k**-4,0)
 
-    c_sp_bar, c_wb_bar, c_br_bar, c_sp, c_wb, c_br_vv, c_br_hh=DopRIM2023_DP(S, k_x, k_y, dks, obs_geo.inc_m, u_10, phi_w, k_r=0)
+    # calculate backscatter TODO check in what system phi_w is expected
+    sigma_los, dsigmadth, q = backscatter_Kudry2023(S, k_x, k_y, dks, phi_w=phi_w, theta=obs_geo.inc_m, u_10=u_10, k_r=k_r,
+                                                    degrees=False)
+    # sigma_los = np.array([sigma_sp, sigma_br_VV, sigma_br_HH, sigma_wb])
+    # calculate backscatter ratios for VV polarization assuming specular is negligeable
+    rat=[0, 
+        sigma_los[-1]/np.sum(sigma_los[1] + sigma_los[-1]),
+        sigma_los[1]/np.sum(sigma_los[1] + sigma_los[-1])]
 
-    return c_sp_bar, c_wb_bar, c_br_bar, c_sp, c_wb, c_br_vv, c_br_hh
+    # calculate velocity of scattering facets
+    c_sp_bar, c_wb_bar, c_br_bar, c_sp, c_wb, c_br_vv, c_br_hh=DopRIM2023_DP(S, k_x, k_y, dks, obs_geo.inc_m, u_10, phi_w, k_r=k_r)
+
+    # calculate surface velocity doppler (from forward_models.Doppler.DopRIM)
+    V = rat[0] * (c_sp_bar + c_sp) + \
+        rat[1] * (c_br_bar + c_br_vv) + \
+        rat[2] * (c_wb_bar + c_wb) 
+    
+
+    return V#, c_sp_bar, c_wb_bar, c_br_bar, c_sp, c_wb, c_br_vv, c_br_hh
