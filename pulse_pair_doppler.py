b4b63e7753acb3f3feaed2b2ac712b3800e48c50
owenodriscoll <owenodriscoll1@gmail.com>
2023-10-17 15:56:27 +0200

    generate reflection from surface, included backscatter slope afo time

--- pulse_pair_doppler.py
+++ pulse_pair_doppler.py
@@ -1,9 +1,10 @@
 from numpy.typing import ArrayLike
 import numpy as np
-from scipy.signal import correlate
+from scipy.signal import correlate, fftconvolve
 from tqdm import tqdm
 from dataclasses import dataclass
 from typing import Callable, Union, List, Dict, Any
+from stereoid.oceans.GMF.cmod5n import cmod5n_forward
 
 
 def chirp(t, f, k_c):
@@ -93,22 +94,26 @@ def delayed_autocorrelation(signal, window_size, step_size):
 
 
 c = 3e8 # m/s
-Lambda = 0.05 #c/f # m
+
 
 @dataclass
 class pulse_pair_doppler:
     t_pulse: Union[int, float]
     t_receive: Union[int, float]
     t_interpulse: Union[int, float]
-    n_reflectors: int #
     n_pulses: int
     n_bursts: int
     bandwidth: Union[int, float]
     baseband: Union[int, float]
+    seed: Union[int, float]
+    n_reflectors: int = None
     oversample_retriev: Union[int, float] = 1
     range_cell_avg_factor: Union[int, float] = 2 # downsample factor after cross correlation
     range_cell_size_frac_pulse: Union[int, float] = 1
-    seed: Union[int, float] = 42
+    Lambda = 0.05 # wavelength
+    R = 700e3 # satellite elevation
+    theta_min = 27 #minimum incidence angle, deg
+
 
     def __post_init__(self):
         self.k_c = self.bandwidth/self.t_pulse # hz/s
@@ -118,6 +123,8 @@ class pulse_pair_doppler:
         self.pulse_samples = int(np.round(self.t_pulse/s))
         self.t_pulse_vector = np.linspace(0, self.t_pulse, self.pulse_samples) # s
         self.receive_samples = int(np.round(self.t_receive*fs ))
+
+        if self.seed == None: self.seed = 42
 
     def chirp(self, centre_around_baseband: bool = True):
         """
@@ -148,12 +155,14 @@ class pulse_pair_doppler:
         return 
     
 
-    def simulate_reflection(self, seed: Union[int,float, bool] = seed, progress_bar_dissable = False):
+    def _simulate_reflection(self, seed: Union[int,float, bool] = True, progress_bar_dissable = False):
         """
 
         """
         # fix randomness for reproducibility
-        if (seed == True) or type(seed) != bool:
+        if seed == True:
+            np.random.seed(self.seed)
+        elif type(seed) != bool:
             np.random.seed(seed)
         
         # instantiate empty receive signal
@@ -165,7 +174,7 @@ class pulse_pair_doppler:
 
             # reflection has random time delay and amplitude
             delta = np.random.randint(low = 0, high = self.receive_samples)
-            A = abs(np.random.rand(1)[0]) + 0.01
+            A = np.random.rayleigh(2, 1)[0] 
             scatterer_phase = np.exp(1j*np.random.rand(1)*2*np.pi)
 
             # for given signals, apply delay and amplitude and combine for total received reflection
@@ -179,6 +188,57 @@ class pulse_pair_doppler:
 
         return 
     
+    # NOTE this does not take into account varying grid resolution as function of incidence angle
+    def simulate_reflection(self, phi_avg: Union[int,float] = 90, v_avg: Union[int,float] = 6, seed: Union[int,float,bool] = True):
+        """
+        input
+        -----
+        phi_avg:
+        v_avg:
+        seed: 
+
+        """
+        # fix randomness for reproducibility
+        if seed == True:
+            np.random.seed(self.seed)
+        elif type(seed) != bool:
+            np.random.seed(seed)
+
+        # compute change in backscatter from CMOD
+        R1 = self.R / np.cos(np.deg2rad(self.theta_min))
+        R2 = self.t_receive * c + R1
+        R_n = np.linspace(R1, R2, self.receive_samples)
+        theta = np.rad2deg(np.arccos(self.R/R_n))
+
+        phi = np.ones(self.receive_samples) * phi_avg
+        v = np.ones(self.receive_samples) * v_avg
+        surface_amplitude_slope = cmod5n_forward(v=v, phi=phi, theta=theta)
+        surface_amplitude_slope /= np.max(surface_amplitude_slope)
+        surface_amplitude = np.random.rayleigh(2, self.receive_samples)
+
+        # generate uniform distribution of phase
+        surface_phase = np.exp(1j*2*np.pi*np.random.rand(self.receive_samples))
+
+        # filter to n_reflectors
+        if self.n_reflectors is not None:
+             
+            n_reflector_filter = np.zeros(self.receive_samples)
+
+            # Generate n unique random indices within the range
+            random_indices = np.random.choice(self.receive_samples, self.n_reflectors, replace=False)
+
+            # Set the selected indices to 1
+            n_reflector_filter[random_indices] = 1
+        else:
+            n_reflector_filter = 1
+
+        self.surface = surface_amplitude * surface_amplitude_slope * surface_phase * n_reflector_filter
+        # self.surface = 1 * 1 * surface_phase * n_reflector_filter
+
+        self.reflections = fftconvolve(self.surface, self.signal, mode = 'valid')
+
+        return 
+    
     def pulse_compress(self, window_function: bool = False):
         """
         
@@ -188,7 +248,7 @@ class pulse_pair_doppler:
         else:
             taper = 1
         
-        self.pulse_compressed = correlate(self.reflections, taper*self.pulse, mode = "same", method = "fft")
+        self.pulse_compressed = correlate(self.reflections, taper*self.pulse, mode = "valid", method = "fft")
 
         return 
     
@@ -204,8 +264,7 @@ class pulse_pair_doppler:
         corr = []
 
         # Calculate delayed correlation
-        for i in tqdm(range(0, len(self.pulse_compressed) - window_size + 1, window_size), desc="Autocorrelation cell", disable=progress_bar_dissable):
-
+        for i in tqdm(range(0, len(self.pulse_compressed) - 2 * window_size + 1, window_size), desc="Autocorrelation cell", disable=progress_bar_dissable):
             # Get the current sample range
             sample_range = self.pulse_compressed[i : i+window_size]
             
@@ -216,7 +275,7 @@ class pulse_pair_doppler:
                 next_sample_range = self.pulse_compressed[i+step_size : i + step_size + window_size]
 
                 # compute correlation between first sample range and next
-                corr_result = correlate(sample_range, next_sample_range, mode = "same", method = "fft")
+                corr_result = correlate(sample_range, next_sample_range, mode = "full", method = "fft")
                 
                 # store results
                 corr.append(corr_result)
@@ -257,7 +316,7 @@ class pulse_pair_doppler:
         phase_diffs_avg_rg = []
         for i in tqdm(range(self.n_bursts), desc="Burst number"):
 
-            self.simulate_reflection(seed=False, progress_bar_dissable = progress_bar_dissable)
+            self.simulate_reflection(seed=False)
             self.pulse_compress(window_function)
             self.delayed_autocorrelation(progress_bar_dissable = progress_bar_dissable)
             self.phase_difference()
