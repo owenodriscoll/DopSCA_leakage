2765b80727048c70e72fcb85b202a609775440ef
owenodriscoll <owenodriscoll1@gmail.com>
2024-01-08 17:45:56 +0100

    added ML prediction function given scene and model data

--- misc.py
+++ misc.py
@@ -1,5 +1,11 @@
 import datetime
 import numpy as np
+import xarray as xr
+import pandas as pd
+import joblib
+import sys
+import io
+import warnings
 
 def round_to_hour(dt):
     dt_start_of_hour = dt.replace(minute=0, second=0, microsecond=0)
@@ -126,3 +132,66 @@ def era5_wind_area(year, month, day, time, latmax, lonmin, latmin, lonmax, filen
         },
         f'{filename}')
     return
+
+
+def warning_catcher_ML(function, *args):
+    """
+    Function to catch all the numerous warnings the ML scripts yield...
+    """
+    with warnings.catch_warnings(record=True) as caught_warnings:
+        output_buffer = io.StringIO()
+        stdout_save = sys.stdout
+        sys.stdout = output_buffer
+
+        try:
+            result = function(*args)
+        except Exception as e:
+            sys.stdout = stdout_save
+            print(e)
+        
+        sys.stdout = stdout_save
+
+    return result
+
+
+def prediction_ML(filename: str, model_ML: str, field_data: str = 'nrcs_scat_subscene_w_noise'):
+    
+    data = xr.open_dataset(filename)
+    model = joblib.load(model_ML)
+
+    # slow time can not be indexed for concatonation step, remove indexing if present
+    try:
+        data = data.reset_index('slow_time')
+    except:
+        pass
+
+    # select samples along azimuth to fill azimuth beam footpint cutoff
+    n = int(data.az_mask_cutoff / data.stride / 2) 
+
+    # select spaced data
+    X_data = xr.concat([data[field_data].isel(slow_time=slice(i-n, 1+i+n)).drop_vars('slow_time') for i in range(n, len(data[field_data]['slow_time'])-n)], dim='placeholder')
+
+    # reshape data and turn in M * (2n + 1) dataframe
+    X_test_pre = X_data.values.reshape(-1, 2 * n+1)
+    df_data = pd.DataFrame(X_test_pre)
+
+    # exclude indexes containing NaN's, these cannot be fed to ML algorithm
+    indexes_non_nan = df_data[~df_data.isna().any(axis = 1)].index.values
+    df_data.dropna(inplace=True)
+
+    # perform predict using a warning catcher
+    pred = warning_catcher_ML(model.predict, df_data)
+    
+    # add prediction to empty field, accounting for indexes
+    empty_field = np.nan*np.ones_like(data[field_data]).ravel()
+    empty_field[indexes_non_nan] = pred
+
+    # reconstruct original shape 
+    result = empty_field.reshape(*data[field_data].T.shape)
+
+    # account for missing "n" rows at the bottom
+    result = np.roll(result, shift = n, axis=0)
+
+    # add to data
+    data['estimate'] = (['grg', 'slow_time'], result.T)
+    return data
