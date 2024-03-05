Fixed bug, leakage now given in LoS

Sentinel1_analyses/velocity_leakage.py
@@ -577,7 +577,7 @@ class S1DopplerLeakage:
         """
         
         self.data['distance_slant_range'] = np.sqrt(self.data['distance_ground']**2 + self.z0**2)
-        self.data['az_angle_wrt_boresight'] = np.arcsin((self.data['distance_az'])/self.data['distance_slant_range']) # incidence from boresight
+        self.data['az_angle_wrt_boresight'] = np.arcsin((self.data['distance_az'])/self.data['distance_slant_range']) # NOTE arcsin instead of tan as distance_slant_range includes azimuthal angle
         self.data['grg_angle_wrt_boresight'] = np.deg2rad(self.data['inc_scatt_eqv'] - self.incidence_angle_scat_boresight)
         self.data = self.data.transpose('az_idx', 'grg', 'slow_time')
 
@@ -605,23 +605,28 @@ class S1DopplerLeakage:
 
     def compute_leakage_velocity(self, add_pulse_pair_uncertainty = True):
         """
-        Computes leakage velocity considering the beam pattern, nrcs weighting and geometric Doppler
+        Computes Line of Sight (LoS) leakage Doppler and velocity considering the beam pattern, nrcs weighting and geometric Doppler
         NOTE range-dependend gain compensation (weight_rg) returns overestimated signal at sidelobe nulls
 
         Parameters
         ----------
         add_pulse_pair_uncertainty : Bool
-            ...
+            whether to add pulse-pair uncertainty following Hoogeboom et al., (2018)
         
 
         """
         # compute geometrical doppler, beam pattern and nrcs weigths
-        self.data['dop_geom'] = (2 * self.vx_sat * np.sin(self.data['az_angle_wrt_boresight']) / self.Lambda) # eq. 4.34 from Digital Procesing of Synthetic Aperture Radar Data by Ian G. Cumming 
+        # NOTE Since rectilienar geometry with flat Earth elevation_angle == incidence angle
+        self.data['elevation_angle'] = np.radians(self.data['inc_scatt_eqv'])
+        # NOTE LOS geometric Doppler
+        self.data['dop_geom'] = 2 / self.Lambda * self.vx_sat * np.sin(self.data['az_angle_wrt_boresight']) * np.sin(self.data['elevation_angle']) # eq. 4.34 from Digital Procesing of Synthetic Aperture Radar Data by Ian G. Cumming 
         self.data['nrcs_weight'] = (self.data['nrcs_scat_eqv'] / self.data['nrcs_scat_eqv'].mean(dim=['az_idx'])) # NOTE weight calculated per azimuth line
 
         # compute weighted received Doppler and resulting apparent LOS velocity
         self.data['dop_beam_weighted'] = self.data['dop_geom'] * self.data['beam'] * self.data['nrcs_weight']
-        self.data['V_leakage'] = self.Lambda * self.data['dop_beam_weighted'] / (2 * np.sin(np.deg2rad(self.data['inc_scatt_eqv']))) # using the equivalent scatterometer incidence angle
+        # NOTE times np.sin(np.pi/2) (=1) because right-looking beam with no squint assumed (no azimuthal angle w.r.t. boresight) 
+        # NOTE LoS leakage
+        self.data['V_leakage'] = self.Lambda / 2 * self.data['dop_beam_weighted'] / ( np.sin(np.pi/2) * np.sin(self.data['elevation_angle'])) # using the equivalent scatterometer incidence angle
 
         # calculate scatt equivalent nrcs
         self.data['nrcs_scat'] = ((self.data['nrcs_scat_eqv'] * self.data['beam']).sum(dim='az_idx', skipna=False) / self.data['beam'].sum(dim='az_idx'))
@@ -632,7 +637,7 @@ class S1DopplerLeakage:
         self.data[['doppler_pulse_rg', 'V_leakage_pulse_rg']] = receive_rg / weight_rg
         
         # add attribute
-        self.data['V_leakage_pulse_rg'] = self.data['V_leakage_pulse_rg'].assign_attrs(units= 'm/s', description = 'Line of sight velocity ')
+        self.data['V_leakage_pulse_rg'] = self.data['V_leakage_pulse_rg'].assign_attrs(units= 'm/s', description = 'Line of Sight velocity ')
 
         # add pulse pair velocity uncertainty
         if (self._pulsepair_noise) & (add_pulse_pair_uncertainty):