077f4df1883a05bdfe857124e88d71c56218b1c5
owenodriscoll <owenodriscoll1@gmail.com>
2024-09-08 21:57:08 -0100

    prepped zero-padding in spatial domain prior to zero padding in frequency domain

--- src/leakage/frequency_domain_padding.py
+++ src/leakage/frequency_domain_padding.py
@@ -9,28 +9,64 @@ def padding_fourier(
 ) -> xr.DataArray:
     """
     Interpolate data by zero-padding in Fourier domain along dimension
+
+    NOTE if input DataArray is discontinuous it may need prior zero-padding in spatial domain
+
     """
     if is_chunked_checker(da):
         da = da.chunk({**da.sizes})
 
+    if isinstance(dimension, str) and isinstance(padding, (int, tuple)):
+        padding_dictionary = {dimension: padding}
+        # da = da.assign_coords({dimension : da[dimension]})
+        # limit = {dimension : slice(da[dimension].min(), da[dimension].max())}
+
+    elif isinstance(dimension, (list, tuple)) and isinstance(padding, int):
+        padding_dictionary = {dim: padding for dim in dimension}
+        # da = da.assign_coords({dim : da[dim] for dim in dimension})
+        # limit = {dim : slice(da[dim].min(),da[dim].max()) for dim in dimension}
+
+    elif isinstance(dimension, (list, tuple)) and isinstance(padding, (list, tuple)):
+        padding_dictionary = {dim: pad for (dim, pad) in zip(dimension, padding)}
+        # da = da.assign_coords({dim : da[dim] for dim in dimension})
+        # limit = {dim : slice(da[dim].min(),da[dim].max()) for dim in dimension}
+
+    padding_dictionary_freq = {"freq_" + k: v for k, v in padding_dictionary.items()}
+    
+    # pre-pad data in spatial domain
+    da_pad = xrft.pad(da,
+        pad_width=padding_dictionary,
+    )
+
     da_spec = xrft.fft(
         da, true_amplitude=False
     )  # set to false for same behaviour as np.fft
+
     da_spec_padded = xrft.padding.pad(
-        da_spec, {"freq_" + dimension: padding}, constant_values=pad_value
+        da_spec, padding_dictionary_freq, constant_values=pad_value
     )
 
     # data is rechunked because Fourier transform cannot be performed over chunked dimension
     if is_chunked_checker(da_spec_padded):
         da_spec_padded = da_spec_padded.chunk({**da_spec_padded.sizes})
 
-    da_spec_padded *= (
-        da_spec_padded.sizes["freq_" + dimension] / da.sizes[dimension]
-    )  # multiply times factor to compensate for more samples in Fourier domain
+    # multiply times factor to compensate for more samples in Fourier domain
+    da_spec_padded *= np.multiply(*da_spec_padded.shape) / np.multiply(*da.shape)
+
     lag = [da_spec_padded[d].attrs.get("direct_lag", 0.0) for d in da_spec_padded.dims]
     da_padded = xrft.ifft(
         da_spec_padded, true_amplitude=False, lag=lag
     )  # set to false for same behaviour as np.fft
+
+    # remove padding in spatial domain
+    # da_padded = da_padded.sel(limit)
+
+    assert_message = ("Average complex component after zero-pad interpolation exceeds 0.1% of real component. "
+                      "Input may be too discontinuous, consider prior zero padding in spatial domain")
+    ratio_4_assert = abs(da_padded.imag).max() / abs(da_padded.real).mean()
+
+    if not np.iscomplex(da).any():
+        assert np.isclose(ratio_4_assert, 0, atol = 1e-4), assert_message 
 
     return da_padded
 
